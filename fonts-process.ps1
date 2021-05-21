e2fd2b8f939b1ed7feb48ff4b09ab8f3a16df257
Joshua Munro <Joshua.Munro@au.logicalis.com>
2021-05-21 17:30:11 +1000

    initial commit

---
+++ fonts-process.ps1
@@ -0,0 +1,28 @@
+$FontsFolder = "$PSScriptroot\fonts"
+
+try {
+    foreach ($font in Get-ChildItem -Path $fontsFolder -File) {
+        $dest = "C:\Windows\Fonts"
+        $fontname = split-path $font -leaf
+        $fontreg = $fontname -replace ".ttf", " (trueType)"
+            try{
+                write-host "copying font $fontreg" -ForegroundColor Green
+                write-host "to destination $destreg" -ForegroundColor Green
+                $font | Copy-Item -Destination $dest
+            }
+            catch{
+                write-error "unable to copy file $fontreg"
+            }
+
+            try{
+                New-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" -Name $fontreg -value $font
+            }
+            catch{
+                write-host "unable to create registry file for $fontreg" -ForegroundColor Green
+            }
+        }
+}
+catch{
+    write-error "General Failure"
+}
+Pause
